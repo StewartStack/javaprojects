40ba7abbc5e8e216805d54277a35ae2ec9bc457b
John Stewart <jstewartstack@gmail.com>
2022-10-01 16:27:21 +0100

    finished

--- MyProfile/src/myprofile/MyProfile.java
+++ MyProfile/src/myprofile/MyProfile.java
@@ -7,14 +7,22 @@ package myprofile;
 /**
  *
  * @author jcls1987
+ * My Profile | Learn Java | Codecademy
+ * 10/1/2022
  */
 public class MyProfile {
 
     /**
      * @param args the command line arguments
+     * @param string TODO
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String firstName = "John ";
+        String lastName = "Stewart ";
+        int age = 35;
+        boolean currentlyEmployed = false;
+        String printOut = firstName + "\n" + lastName + "\n" + age + "\n" + currentlyEmployed;
+        System.out.println(printOut);
     }
-    
+
 }
