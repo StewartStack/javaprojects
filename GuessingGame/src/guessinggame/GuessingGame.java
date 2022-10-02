af81464cc4a2429027ba1975dd15dbedd5548dcc
John Stewart <jstewartstack@gmail.com>
2022-10-02 22:06:30 +0100

    finished

---
+++ GuessingGame/src/guessinggame/GuessingGame.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guessinggame;
+
+/**
+ *
+ * @author jcls1987
+ * Guessing Game | Learn Java | Codecademy
+ * 10/02/2022
+ */
+public class GuessingGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int mystery1 = 8 + 6;
+        int mystery2 = 8 - 6;
+        System.out.println(mystery2);
+    }
+
+}
